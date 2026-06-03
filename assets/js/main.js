1d960ef8c0bed505acde0912e1ba6ae7bc9f7bf8
Daniel Motejadded <danimotejadded@gmail.com>
2026-06-03 02:53:52 +0200

    Add new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ const { chars: chars2 } = splitText(".home__profession-2", {
 
 animate(chars1, {
   y: [{ to: ["100%", "0%"] }, { to: "-100%", delay: 4000, ease: "in(3)" }],
-  duration: 700,
+  duration: 900,
   ease: "out(3)",
   delay: stagger(80),
   loop: true,
@@ -23,7 +23,6 @@ animate(chars2, {
   delay: stagger(80),
   loop: true,
 });
-
 
 /*=============== SWIPER PROJECTS ===============*/
 
