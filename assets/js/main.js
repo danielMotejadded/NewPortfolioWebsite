6fa60866be784ecf0c302eaf3adb822eabdccdbc
Daniel Motejadded <danimotejadded@gmail.com>
2026-06-07 19:29:52 +0200

    Add another sections to portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,7 @@ const swiperProjects = new Swiper(".projects__swiper", {
   speed: 600,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   autoplay: {
     delay: 3000,
@@ -41,18 +42,93 @@ const swiperProjects = new Swiper(".projects__swiper", {
 });
 /*=============== WORK TABS ===============*/
 
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const targetSelector = tab.dataset.target;
+    console.log(targetSelector);
+    const targetContent = document.querySelector(targetSelector);
+
+    tabContents.forEach((content) => content.classList.remove("work-active"));
+    tabs.forEach((t) => t.classList.remove("work-active"));
+
+    tab.classList.add("work-active");
+    targetContent.classList.add("work-active");
+  });
+});
 /*=============== SERVICES ACCORDION ===============*/
 
 /*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
 
 /*=============== COPY EMAIL IN CONTACT ===============*/
+const copyBtn = document.getElementById("contact-btn");
+const copyEmail = document.getElementById("contact-email").textContent;
 
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard
+    .writeText(copyEmail)
+    .then(
+      () => (copyBtn.innerHTML = 'Email copied <i class="ri-check-line"></i>'),
+    );
+  setTimeout(() => {
+    copyBtn.innerHTML = ' Copy email <i class="ri-file-copy-line"></i>';
+  }, 2000);
+});
 /*=============== CURRENT YEAR OF THE FOOTER ===============*/
+const year = new Date().getFullYear();
+const yearSpan = document.querySelector("#footer-year");
+yearSpan.innerHTML = year;
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((section) => {
+    const id = section.id,
+      top = section.offsetTop - 50,
+      height = section.offsetHeight,
+      link = document.querySelector(".nav__menu a[href*=" + id + "]");
+
+    if (!link) return;
+    link.classList.toggle(
+      "active-link",
+      scrollY > top && scrollY <= top + height,
+    );
+  });
+};
+window.addEventListener("scroll", scrollActive);
 /*=============== CUSTOM CURSOR ===============*/
+const cursor = document.querySelector(".cursor");
+let mouseX = 0,
+  mouseY = 0;
 
+const cursorMove = () => {
+  cursor.style.left = `${mouseX}px`;
+  cursor.style.top = `${mouseY}px`;
+  cursor.style.transform = "translate(-50%, -50%)";
+  requestAnimationFrame(cursorMove);
+};
+
+document.addEventListener("mousemove", (e) => {
+  console.log("Hello");
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+cursorMove();
 /* Hide custom cursor on links */
+
+const a = document.querySelectorAll("a");
+a.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    cursor.classList.add("hide-cursor");
+  });
+  item.addEventListener("mouseleave", () => {
+    cursor.classList.remove("hide-cursor");
+  });
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
